a6847f78041f1a547b1e6b5f075c5a0105267b96
Lawrence <lawrencedawson@hotmail.co.uk>
2016-08-10 12:26:19 +0100

    completed fizzbuzz game

---
+++ spec/fizzbuzz_spec.rb
@@ -0,0 +1,28 @@
+require 'fizzbuzz'
+
+describe 'fizzbuzz' do
+  it 'returns."fizz" when passed 3' do
+    expect(fizzbuzz(3)).to eq 'fizz'
+  end
+
+  it 'returns."buzz" when passed 5' do
+    expect(fizzbuzz(5)).to eq 'buzz'
+  end
+
+  it 'returns "fizz" when passed a multiple of 3' do
+    expect(fizzbuzz(6)).to eq 'fizz'
+  end
+
+  it 'returns."buzz" when passed multiples of 5' do
+    expect(fizzbuzz(10)).to eq 'buzz'
+  end
+
+  it 'returns."original_number" when original number not divisable by 3 or 5' do
+    expect(fizzbuzz(4)).to eq '4'
+  end
+
+  it 'returns."fizzbuzz" when passed multiple of 3 and 5' do
+    expect(fizzbuzz(15)).to eq 'fizzbuzz'
+  end
+
+end
